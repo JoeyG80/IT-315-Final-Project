c0d5d7c933248a3d64d16afe4a812c17f920a34e
user134481 <user134481@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-05-10 22:05:29 -0400

    Added Calculator functionality

--- FitEZ_Tuesday_Spring_2018/CalculatorViewController.swift
+++ FitEZ_Tuesday_Spring_2018/CalculatorViewController.swift
@@ -8,8 +8,47 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+class CalculatorViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var PerimeterLength1: UITextField!
+    @IBOutlet weak var PerimeterWidth1: UITextField!
+    @IBOutlet weak var PerimeterTotal: UITextView!
+    @IBOutlet weak var PerimeterCalculate: UIButton!
+    
+    @IBAction func goToPerimeter(_ sender: UIButton) {
+        let YARD = 36
+        let SIDES = 2
+        let length  = (PerimeterLength1.text! as NSString).integerValue
+        let width = (PerimeterWidth1.text! as NSString).integerValue
+        let perimeter = ((length * SIDES) + (width * SIDES)) / YARD
+        PerimeterTotal.text = String(perimeter) + " yard(s)"
+    }
+    
+    
+    @IBOutlet weak var PerimeterLength2: UITextField!
+    @IBOutlet weak var PerimeterWidth2: UITextField!
+    @IBOutlet weak var Answer: UITextView!
+    @IBOutlet weak var calculateNumSquares: UIButton!
+    
+    @IBAction func goToNumSquares(_ sender: UIButton) {
+        let length  = (PerimeterLength1.text! as NSString).integerValue
+        let width = (PerimeterWidth1.text! as NSString).integerValue
+        let length2  = (PerimeterLength2.text! as NSString).integerValue
+        let width2 = (PerimeterWidth2.text! as NSString).integerValue
+        let area1 = length * width
+        let area2 = length2 * width2
+        if(!(area1 == 0) && !(area2 == 0)){
+            let numSquares = area1 / area2
+            if((area1 % area2) > 0){
+                Answer.text = "The number of squares does not go evenly"
+            }
+            else{
+                Answer.text = String(numSquares) + " square(s) will"
+            }
+        }
+    }
+
+    
     
     @IBAction func onGoBack(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -21,7 +60,11 @@ class CalculatorViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
